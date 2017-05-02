0a2d712400c24dc1697091c462d428087ca4d1e0
Austin Bingham <austin@sixty-north.com>
2017-05-02 17:14:14 +0100

    Added some pytest plugin command-line options.

--- testing_board/pytest.py
+++ testing_board/pytest.py
@@ -2,6 +2,23 @@ failures = 0
 count = 0
 
 
+def pytest_addoption(parser):
+    parser.addoption("--testing_board:port",
+                     action="store",
+                     default="8080",
+                     help="port of testing_board server")
+    parser.addoption("--testing_board:host",
+                     action="store",
+                     default="localhost",
+                     help="host of testing_board server")
+    parser.addoption("--testing_board:game",
+                     action="store",
+                     help="game id")
+    parser.addoption("--testing_board:user",
+                     action="store",
+                     help="user id")
+
+
 def pytest_runtest_logreport(report):
     global count
     global failures
@@ -12,4 +29,7 @@ def pytest_runtest_logreport(report):
 
 
 def pytest_sessionfinish(session, exitstatus):
+    print('HOST:', session.config.getoption("--testing_board:host"))
+    print('PORT:', session.config.getoption("--testing_board:port"))
     print('PLUGIN: {} failures / {} tests'.format(failures, count))
+    # TODO: Post results to testing_board server
